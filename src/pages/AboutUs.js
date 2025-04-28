303960a30a0b09b60770ec75772679a4c11fe64e
theminda <renuka.kirulapana@gmail.com>
2025-04-28 15:31:24 +0530

    mapp adedd

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -96,9 +96,40 @@ function AboutUs() {
                         </p>
                     </div>
                 </div>
-               
+
             </div>
-            
+            <div className='container-fluid mb-5' >
+                <h3 style={{ textAlign: 'center', fontSize: '30px', color: 'GrayText' }}>Jetwing Honors member benefits</h3>
+
+                <div className="card" style={{ width: '21rem', marginTop: '40px', marginLeft: 'auto', marginRight: 'auto' }}>
+
+                    <div className="card-body }>">
+                        <h5 className="card-title" style={{color:'purple'}}>Member Benefits</h5>
+                        <p className="card-text">
+                            <li>Hilton Honors Discount rate</li>
+                            <li>Points toward free nights </li>
+                            <li>Choose Your Room</li>
+                            <li>Digital Check-In</li>
+                            <li>Hilton Honors Experiences</li>
+                        </p>
+                    </div>
+                </div>
+            </div>
+
+            <div className='container-fluid mb-5'>
+                <h3 style={{ textAlign: 'center', fontSize: '30px', color: 'GrayText' }}>MAP in Get in Touch</h3>
+                <div style={{ textAlign: 'center' }}>
+                    <iframe 
+                        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d124051.0903472799!2d79.9704319!3d6.9225949!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3ae259c22d2f2617%3A0x773eebfe40a5a849!2sJetwing%20Colombo%207!5e0!3m2!1sen!2slk!4v1684327259460!5m2!1sen!2slk"
+                        width="50%" 
+                        height="450" 
+                        style={{ border: 0 }} 
+                        allowFullScreen="" 
+                        loading="lazy" 
+                        referrerPolicy="no-referrer-when-downgrade">
+                    </iframe>
+                </div>
+            </div>
 
         </div>
 
