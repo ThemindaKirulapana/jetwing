b86f8f73b1cb29d92db91ad10a41f803ec1bcaf4
theminda <renuka.kirulapana@gmail.com>
2025-04-28 15:41:51 +0530

    footer updated

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,25 +3,18 @@ import React from 'react'
 function Footer() {
   return (
     <footer className="bg-black text-white px-6 md:px-26 lg:px-28 py-15">
-    {/* <div className="grid grid-cols-1 md:grid-cols-3 gap-8 mt-10">
+    <div className="grid grid-cols-1 md:grid-cols-3 gap-8 mt-10 ">
       
-      <div>
-        <h2 className="text-2xl font-bold mb-4">About Us</h2>
-        <p className="text-gray-300">
-          We are committed to delivering world-class hospitality, unforgettable experiences, and sustainable tourism.
-          Discover Sri Lanka's beauty with us and create memories that will last a lifetime.
-        </p>
-      </div>
- 
-      <div>
+      
+      <div style={{ marginTop: '20px',marginLeft: '20px' }}>
         <h2 className="text-2xl font-bold mb-4">Contact</h2>
         <p className="text-gray-300">123, Jetwing Street, Colombo, Sri Lanka</p>
-        <p className="text-gray-300 mt-2">Phone: +94 11 2345678</p>
+        <p className="text-gray-300 mt-2">Phone: +94 11 2345678/ + 94 11 2550200</p>
         <p className="text-gray-300">Email: info@jetwinghotels.com</p>
       </div>
-    </div> */}
+    </div>
   
-    <div className="text-center text-gray-400 mt-10">
+    <div className="text-center text-gray-400 mt-10 border-t border-gray-700 pt-4">
       © 2025 Jetwing Hotels. All rights reserved.
     </div>
   </footer>
